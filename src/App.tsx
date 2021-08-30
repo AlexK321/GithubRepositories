998d1678df6f9a3b547ae2739e3ec6c2e6acee04
Alexey Kostevich <a.kostevich@itransition.com>
2021-08-30 14:23:13 +0300

    internationalisation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import AuthContext, { initialState } from './AuthContext';
 import styles from './App.module.scss';
 import Repositories from './components/Repositories/Repositories';
 import TrackingHead from './components/TrackingHead/TrackingHead';
+import CubeLoader from './components/CubeLoader/CubeLoader';
 import Login from './components/Login/Login';
 import Repository from './components/Repository/Repository';
 import Layout from './components/Layout/Layout';
+import './utils/i18next';
 
 export interface IInitialState {
   isLoggedIn: Boolean | string;
@@ -22,25 +24,27 @@ const App: React.FC = () => {
   const [authState, setAuthState] = useState<IInitialState>(initialState);
 
   return (
-    <AuthContext.Provider
-      value={{
-        authState,
-        setAuthState,
-      }}
-    >
-      <div className={styles.App} ref={app}>
-        <TrackingHead app={app} />
-        <Router>
-          <Switch>
-            <Route component={Repositories} exact path="/" />
-            <Route component={Login} exact path="/login" />
-            <Route component={Repository} exact path="/repository" />
-            <Route component={Layout} exact path="/layout" />
-            <Redirect to="/layout" />
-          </Switch>
-        </Router>
-      </div>
-    </AuthContext.Provider>
+    <Suspense fallback={<CubeLoader />}>
+      <AuthContext.Provider
+        value={{
+          authState,
+          setAuthState,
+        }}
+      >
+        <div className={styles.App} ref={app}>
+          <TrackingHead app={app} />
+          <Router>
+            <Switch>
+              <Route component={Repositories} exact path="/" />
+              <Route component={Login} exact path="/login" />
+              <Route component={Repository} exact path="/repository" />
+              <Route component={Layout} exact path="/layout" />
+              <Redirect to="/layout" />
+            </Switch>
+          </Router>
+        </div>
+      </AuthContext.Provider>
+    </Suspense>
   );
 };
 
--- src/components/Repository/Repository.tsx
+++ src/components/Repository/Repository.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from '@apollo/client';
+import { MenuItem, Select } from '@material-ui/core';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import GET_REPOSITORY from '../../graphql/query/repository';
 
 const Repository: React.FC = () => {
+  const { t, i18n } = useTranslation();
   const url = new URL(window.location.href);
 
   const repositoryName = url.searchParams.get('name');
@@ -12,14 +15,33 @@ const Repository: React.FC = () => {
     variables: { name: repositoryName, owner: repositoryOwner },
   });
 
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    i18n.changeLanguage(event.target.value as string);
+  };
+
   return (
     <div style={{ paddingTop: '100px' }}>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        onChange={handleChange}
+        value="Ru"
+      >
+        <MenuItem value="en">En</MenuItem>
+        <MenuItem value="ru">Ru</MenuItem>
+      </Select>
       <p>ID: {data?.repository.id}</p>
-      <p>NAME: {data?.repository.name}</p>
+      <p>
+        {t('repository.name')}: {data?.repository.name}
+      </p>
       URL: <a href={data?.repository.url}>{data?.repository.url}</a>
-      <p>description: {data?.repository.description}</p>
+      <p>
+        {t('repository.description')}: {data?.repository.description}
+      </p>
       <p>stargazerCount: {data?.repository.stargazerCount}</p>
-      <p>isPrivate: {String(data?.repository.isPrivate)}</p>
+      <p>
+        {t('repository.isPrivate')}: {String(data?.repository.isPrivate)}
+      </p>
     </div>
   );
 };
